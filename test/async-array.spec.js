360381e1ac89d367f9494899a8c733c07dad5ac0
Sykander <scanda@live.co.uk>
2019-12-27 00:58:42 +0000

    Async Sort Specs - Added test for integration with async array object

--- test/async-array.spec.js
+++ test/async-array.spec.js
@@ -6,16 +6,19 @@ context('Async Array', () => {
 		it('Should inherit from Array', () =>
 			expect(AsyncArray.prototype).to.be.instanceOf(Array));
 
-		it('Should have access asyncFilter method', () =>
+		it('Should have access to the asyncFilter method', () =>
 			expect(AsyncArray.prototype.asyncFilter).to.be.a('function'));
 
-		it('Should have access asyncFind method', () =>
+		it('Should have access to the asyncFind method', () =>
 			expect(AsyncArray.prototype.asyncFind).to.be.a('function'));
 
-		it('Should have access asyncFindIndex method', () =>
+		it('Should have access to the asyncFindIndex method', () =>
 			expect(AsyncArray.prototype.asyncFindIndex).to.be.a('function'));
 
-		it('Should have access asyncMap method', () =>
+		it('Should have access to the asyncMap method', () =>
 			expect(AsyncArray.prototype.asyncMap).to.be.a('function'));
+
+		it('Should have access to the asyncSort method', () =>
+			expect(AsyncArray.prototype.asyncSort).to.be.a('function'));
 	});
 });
